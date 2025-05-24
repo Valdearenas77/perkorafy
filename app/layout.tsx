39911544373433fd8c92290f8cc02616e49e1091
Jose Antonio Fernández Valdearenas <valdearenas77@Joses-Laptop.local>
2025-05-24 12:00:29 +0200

    Agrega <Toaster /> para habilitar los toast de sonner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
-
-import "@/styles/globals.css";
+import "@/styles/globals.css"
+import { Toaster } from "sonner"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Toaster /> {/* Esto permite mostrar los toast */}
+      </body>
     </html>
-  );
+  )
 }
