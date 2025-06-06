085036413e96ad4bf3a1282b595805bb264f9de1
Jose Antonio Fernández Valdearenas <valdearenas77@Joses-Laptop.local>
2025-06-06 19:38:40 +0200

    Fix SMTP config en mailer.ts

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false, // true si usas puerto 465, false para 587
+  secure: true, // true si usas puerto 465, false para 587
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
