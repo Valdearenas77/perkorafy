b41da2fee67c0779c4c8c3a2d4fbd922c2cbe375
Jose Antonio Fernández Valdearenas <valdearenas77@Joses-Laptop.local>
2025-06-11 21:07:41 +0200

    Corrección final: prisma.passwordRecovery en camelCase

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -5,7 +5,7 @@ export async function generarTokenRecuperacion(userId: number) {
   const token = crypto.randomBytes(32).toString('hex')
   const expiracion = new Date(Date.now() + 1000 * 60 * 60 * 24) // 24 horas de validez
 
-  await prisma.PasswordRecovery.create({
+  await prisma.passwordRecovery.create({
     data: {
       usuarioId: userId,
       token,
