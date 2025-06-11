8bb56bf929189483d92af8e834936abd6152c075
Jose Antonio Fernández Valdearenas <valdearenas77@Joses-Laptop.local>
2025-06-11 21:05:54 +0200

    Corregido nombre de modelo PasswordRecovery en prisma client

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -5,7 +5,7 @@ export async function generarTokenRecuperacion(userId: number) {
   const token = crypto.randomBytes(32).toString('hex')
   const expiracion = new Date(Date.now() + 1000 * 60 * 60 * 24) // 24 horas de validez
 
-  await prisma.passwordRecovery.create({
+  await prisma.PasswordRecovery.create({
     data: {
       usuarioId: userId,
       token,
